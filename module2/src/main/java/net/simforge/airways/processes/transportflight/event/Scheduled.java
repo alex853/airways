81350d83bfe7e22bb5ff2970147e59ed463f4ade
alexey853 <alexxxey.kornev@gmail.com>
2019-08-29 19:29:05 +0400

    TransportFlight logics

--- module2/src/main/java/net/simforge/airways/processes/transportflight/event/Scheduled.java
+++ module2/src/main/java/net/simforge/airways/processes/transportflight/event/Scheduled.java
@@ -2,10 +2,6 @@
  * Airways Project (c) Alexey Kornev, 2015-2019
  */
 
-/*
- * Airways Project (c) Alexey Kornev, 2015-2019
- */
-
 package net.simforge.airways.processes.transportflight.event;
 
 import net.simforge.airways.engine.event.Event;
