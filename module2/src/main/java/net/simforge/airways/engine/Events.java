876c493f19cfe1df5189a43aa250ab989aa9ac36
alexey853 <alexxxey.kornev@gmail.com>
2019-08-29 15:10:27 +0400

    Cleanup

--- module2/src/main/java/net/simforge/airways/engine/Events.java
+++ module2/src/main/java/net/simforge/airways/engine/Events.java
@@ -2,10 +2,12 @@
  * Airways Project (c) Alexey Kornev, 2015-2019
  */
 
+/*
+ * Airways Project (c) Alexey Kornev, 2015-2019
+ */
+
 package net.simforge.airways.engine;
 
-import net.simforge.airways.model.flight.TransportFlight;
-import net.simforge.airways.processes.transportflight.event.CheckinClosed;
 import net.simforge.commons.hibernate.BaseEntity;
 
 import java.time.LocalDateTime;
