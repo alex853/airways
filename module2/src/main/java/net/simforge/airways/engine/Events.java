ad8a6b84ce753a3d3d935d78005db84e9b11b5f7
alexey853 <alexxxey.kornev@gmail.com>
2019-08-29 15:17:52 +0400

    Cleanup

--- module2/src/main/java/net/simforge/airways/engine/Events.java
+++ module2/src/main/java/net/simforge/airways/engine/Events.java
@@ -2,16 +2,13 @@
  * Airways Project (c) Alexey Kornev, 2015-2019
  */
 
-/*
- * Airways Project (c) Alexey Kornev, 2015-2019
- */
-
 package net.simforge.airways.engine;
 
 import net.simforge.commons.hibernate.BaseEntity;
 
 import java.time.LocalDateTime;
 
+@Deprecated
 public class Events {
     public static void schedule(Class clazz, BaseEntity entity) {
     }
