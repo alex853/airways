c490ea5f6964f03b9940e86098c7a617259cfe85
alex853 <alexxxey.kornev@gmail.com>
2024-12-13 19:46:34 +0200

    added LatLong16bit type

--- src/main/java/net/simforge/airways2/storage/DataType.java
+++ src/main/java/net/simforge/airways2/storage/DataType.java
@@ -5,8 +5,8 @@ public enum DataType {
 //    Unsigned24bit,
     Unsigned16bit, // todo ak not implemented, not tested
     Unsigned8bit,
-    LatLong24bit, // accuracy 0.0001
-    LatLong16bit, // accuracy 0.01
+    LatLong24bit, // accuracy 0.0001, this means 11 meters step on equator
+    LatLong16bit, // accuracy 0.01, this means 1.1 kilometer step on equator
     PlainString, // todo ak no longer than 254!!!
 //    PackedTo6BitsString
 }
