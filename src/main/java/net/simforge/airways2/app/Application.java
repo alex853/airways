72bdca3842fef9042a0c9fa02540f93944bc2eeb
alex853 <alexxxey.kornev@gmail.com>
2025-05-21 21:49:47 +0300

    Running world builder steps

--- src/main/java/net/simforge/airways2/app/Application.java
+++ src/main/java/net/simforge/airways2/app/Application.java
@@ -3,11 +3,26 @@ package net.simforge.airways2.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        if (args.length == 1) {
+            runWorldBuilderStep(args[0]);
+            return;
+        }
+
         SpringApplication.run(Application.class, args);
+    }
+
+    private static void runWorldBuilderStep(final String stepName) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        final String className = "net.simforge.airways2.worldbuilder." + stepName;
+        final Class stepClass = Class.forName(className);
+        final Method main = stepClass.getMethod("main", String[].class);
+        main.invoke(null, null);
     }
 
 }
