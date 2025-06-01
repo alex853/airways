d8e3c0fa1e4f1651f196d6245cb07fdbbba8e870
alex853 <alexxxey.kornev@gmail.com>
2025-06-01 22:46:47 +0300

    admin-fix - reset aircraft status

--- src/main/java/net/simforge/airways2/app/AdminFixController.java
+++ src/main/java/net/simforge/airways2/app/AdminFixController.java
@@ -17,7 +17,7 @@ public class AdminFixController {
     @Autowired
     private WorldRunnerBean worldBean;
 
-    @GetMapping("/cancel-flight")
+    @GetMapping("/flight/cancel")
     public EnhancedFlightMissionDto cancelFlight(@RequestParam(name = "flightId") final int flightId) {
         return worldBean.modifySync(world -> {
             final FlightMissions.Mission mission = world.flightMissions().byId(flightId).orElseThrow();
@@ -40,6 +40,19 @@ public class AdminFixController {
                 aircraft.setOperationalStatus(Aircrafts.OperationalStatus.Idle);
                 aircraft.setFlightMissionId(0);
             }
+            return null;
+        });
+    }
+
+    @GetMapping("/aircraft/reset-status")
+    public EnhancedFlightMissionDto resetAircraftStatus(@RequestParam(name = "aircraftId") final int aircraftId) {
+        return worldBean.modifySync(world -> {
+            final Aircrafts.Aircraft aircraft = world.aircrafts().byId(aircraftId).orElseThrow();
+
+            aircraft.setLocationStatus(Aircrafts.LocationStatus.ParkedAtAirport);
+            aircraft.setOperationalStatus(Aircrafts.OperationalStatus.Idle);
+            aircraft.setFlightMissionId(0);
+
             return null;
         });
     }
