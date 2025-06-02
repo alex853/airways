822dac93033c1ec420e26d5cb42c5a808931e8d6
alex853 <alexxxey.kornev@gmail.com>
2025-06-02 09:38:24 +0300

    flight board page tuning

--- src/main/java/net/simforge/airways2/app/EnhancedFlightMissionDto.java
+++ src/main/java/net/simforge/airways2/app/EnhancedFlightMissionDto.java
@@ -3,6 +3,7 @@ package net.simforge.airways2.app;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import net.simforge.airways2.world.World;
+import net.simforge.airways2.world.datamodel.Aircrafts;
 import net.simforge.airways2.world.datamodel.FlightMissions;
 
 @Data
@@ -11,6 +12,7 @@ class EnhancedFlightMissionDto {
     private int id;
     private int acId;
     private String acReg;
+    private String acType;
     private String st;
     private String dep;
     private String dest;
@@ -25,10 +27,12 @@ class EnhancedFlightMissionDto {
     public static EnhancedFlightMissionDto fromMission(
             final World world,
             final FlightMissions.Mission mission) {
+        final Aircrafts.Aircraft aircraft = world.aircrafts().byId(mission.getAircraftId()).orElseThrow();
         return new EnhancedFlightMissionDto(
                 mission.getId(),
                 mission.getAircraftId(),
-                world.aircrafts().byId(mission.getAircraftId()).orElseThrow().getRegNo(),
+                aircraft.getRegNo(),
+                world.aircraftTypes().byId(aircraft.getAircraftTypeId()).orElseThrow().getIcao(),
                 mission.getStatus().name(),
                 world.airports().byId(mission.getDepartureAirportId()).orElseThrow().getIcao(),
                 world.airports().byId(mission.getDestinationAirportId()).orElseThrow().getIcao(),
