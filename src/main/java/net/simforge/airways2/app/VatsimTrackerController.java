0752164cb1995bf9ae226409b47031a40959e290
alex853 <alexxxey.kornev@gmail.com>
2025-06-10 10:18:09 +0300

    vatsim tracker - track tail to be shown on the web

--- src/main/java/net/simforge/airways2/app/VatsimTrackerController.java
+++ src/main/java/net/simforge/airways2/app/VatsimTrackerController.java
@@ -80,7 +80,7 @@ public class VatsimTrackerController {
         private String cpType;
         private String cpDep;
         private String cpDest;
-        private String lastTrackedDistance;
+        private String trackTrail;
         private String removal;
         private int fmId;
         private String fmStatus;
