1a4ece868513bc603080c8e6293acadbff4cb9d7
alex853 <alexxxey.kornev@gmail.com>
2025-06-10 21:41:55 +0300

    vatsim tracker - ellipse criterion tuned

--- src/main/java/net/simforge/airways2/app/vatsimtracker/EllipseCriterion.java
+++ src/main/java/net/simforge/airways2/app/vatsimtracker/EllipseCriterion.java
@@ -3,28 +3,31 @@ package net.simforge.airways2.app.vatsimtracker;
 import net.simforge.commons.misc.Geo;
 import net.simforge.networkview.core.Position;
 
-public class EllipseCriterion {
+import java.text.DecimalFormat;
 
-    private final double dist;
+public class EllipseCriterion {
+    private static final DecimalFormat df1 = new DecimalFormat("#.#");
+    private final double distanceBetweenAirports;
+    private final double airportRange = 50;
     private final double c;
     private final double a;
     private final double b;
-    private final double summedDist;
+    private final double sumOfDistancesToPosition;
     private final boolean withinEllipse;
 
     public EllipseCriterion(final Geo.Coords departureCoords, final Geo.Coords destinationCoords, final Position newPosition) {
         final Geo.Coords positionCoords = newPosition.getCoords();
 
-        dist = Geo.distance(departureCoords, destinationCoords);
+        distanceBetweenAirports = Geo.distance(departureCoords, destinationCoords);
 
         // https://ru.wikipedia.org/wiki/%D0%AD%D0%BB%D0%BB%D0%B8%D0%BF%D1%81
-        c = dist / 2;
-        a = c + 100; // 100 nm
+        c = distanceBetweenAirports / 2;
+        a = c + airportRange;
         b = Math.sqrt(a * a - c * c);
 
-        summedDist = Geo.distance(departureCoords, positionCoords) + Geo.distance(positionCoords, destinationCoords);
+        sumOfDistancesToPosition = Geo.distance(departureCoords, positionCoords) + Geo.distance(positionCoords, destinationCoords);
 
-        withinEllipse = summedDist <= 2 * a;
+        withinEllipse = sumOfDistancesToPosition <= 2 * a;
     }
 
     public boolean isWithinEllipse() {
@@ -34,12 +37,13 @@ public class EllipseCriterion {
     @Override
     public String toString() {
         return "EllipseCriterion{" +
-                "dist=" + dist +
-                ", c=" + c +
-                ", a=" + a +
-                ", b=" + b +
-                ", summedDist=" + summedDist +
-                ", withinEllipse=" + withinEllipse +
+                "distanceBetweenAirports=" + df1.format(distanceBetweenAirports) +
+                ", airportRange=" + df1.format(airportRange) +
+                ", c=" + df1.format(c) +
+                ", a(half-length)=" + df1.format(a) +
+                ", b(half-width)=" + df1.format(b) +
+                ", sumOfDistancesToPosition=" + sumOfDistancesToPosition +
+                ", withinEllipse(sum<=2*a)=" + withinEllipse +
                 '}';
     }
 }
