b9679f58d4fb7be9bed09724ad84513fb5d5962f
alex853 <alexxxey.kornev@gmail.com>
2025-06-11 21:53:48 +0300

    /aircraft/flying - details added

--- src/main/java/net/simforge/airways2/app/AircraftController.java
+++ src/main/java/net/simforge/airways2/app/AircraftController.java
@@ -3,6 +3,7 @@ package net.simforge.airways2.app;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import net.simforge.airways2.world.datamodel.Aircrafts;
+import net.simforge.airways2.world.datamodel.FlightMissions;
 import net.simforge.airways2.world.processors.FlightMissionHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -41,14 +42,22 @@ public class AircraftController {
     public List<FlyingAircraftDto> getFlying() {
         return worldBean.read(world ->  world.aircrafts().all().stream()
                 .filter(a -> a.getLocationStatus() == Aircrafts.LocationStatus.Flying)
-                .map(a -> new FlyingAircraftDto(
-                        a.getId(),
-                        world.aircraftTypes().byId(a.getAircraftTypeId()).orElseThrow().getIcao(),
-                        a.getRegNo(),
-                        FlightMissionHelper.formatRoute(world, a.getFlightMissionId()),
-                        FlightMissionHelper.calculateHeading(world, a.getFlightMissionId()),
-                        a.getLocationLatitude(),
-                        a.getLocationLongitude()))
+                .map(a -> {
+                    final FlightMissions.Mission mission = world.flightMissions().byId(a.getFlightMissionId()).orElseThrow();
+                    return new FlyingAircraftDto(
+                            a.getId(),
+                            world.aircraftTypes().byId(a.getAircraftTypeId()).orElseThrow().getIcao(),
+                            a.getRegNo(),
+                            FlightMissionHelper.calculateHeading(world, a.getFlightMissionId()),
+                            a.getLocationLatitude(),
+                            a.getLocationLongitude(),
+                            world.airports().byId(mission.getDepartureAirportId()).orElseThrow().getIcao(),
+                            world.airports().byId(mission.getDestinationAirportId()).orElseThrow().getIcao(),
+                            WebTime.hhmmOrNull(mission.getPlannedDepartureWorldTime()),
+                            WebTime.hhmmOrNull(mission.getPlannedArrivalWorldTime()),
+                            WebTime.hhmmOrNull(mission.getActualTakeoffWorldTime()),
+                            "n/a");
+                })
                 .toList());
     }
 
@@ -72,11 +81,16 @@ public class AircraftController {
     @AllArgsConstructor
     private static class FlyingAircraftDto {
         private int id;
-        private String type;
-        private String regNo;
-        private String route;
-        private float hdg;
+        private String acType;
+        private String acReg;
         private float lat;
         private float lon;
+        private float hdg;
+        private String fpDep;
+        private String fpDest;
+        private String pDep;
+        private String pArr;
+        private String aTof;
+        private String eLdg;
     }
 }
