481f0d2ab4b369bd85b266a3c82644b52df4ddd8
alex853 <alexxxey.kornev@gmail.com>
2025-06-19 10:36:27 +0300

    map improvements

--- src/main/java/net/simforge/airways2/app/AircraftController.java
+++ src/main/java/net/simforge/airways2/app/AircraftController.java
@@ -2,6 +2,7 @@ package net.simforge.airways2.app;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import net.simforge.airways2.app.tools.Timing;
 import net.simforge.airways2.world.datamodel.Aircrafts;
 import net.simforge.airways2.world.datamodel.FlightMissions;
 import net.simforge.airways2.world.processors.FlightMissionHelper;
@@ -22,43 +23,47 @@ public class AircraftController {
 
     @GetMapping("/all")
     public List<FullAircraftDto> getAll() {
-        return worldBean.read(world -> world.aircrafts().all().stream()
-                .map(a -> new FullAircraftDto(
-                        a.getId(),
-                        world.aircraftTypes().byId(a.getAircraftTypeId()).orElseThrow().getIcao(),
-                        a.getRegNo(),
-                        a.getAircraftOperatorId(),
-                        a.getFlightMissionId(),
-                        a.getFlightMissionId() != 0 ? FlightMissionHelper.formatRouteOrNull(world, a.getFlightMissionId()) : null,
-                        a.getOperationalStatus().name(),
-                        a.getLocationStatus().name(),
-                        a.getLocationAirportId() != 0 ? world.airports().byId(a.getLocationAirportId()).orElseThrow().getIcao() : null,
-                        a.getLocationLatitude(),
-                        a.getLocationLongitude()))
-                .toList());
+        try (final Timing.Timer ignored = Timing.label("AircraftController - getAll")) {
+            return worldBean.read(world -> world.aircrafts().all().stream()
+                    .map(a -> new FullAircraftDto(
+                            a.getId(),
+                            world.aircraftTypes().byId(a.getAircraftTypeId()).orElseThrow().getIcao(),
+                            a.getRegNo(),
+                            a.getAircraftOperatorId(),
+                            a.getFlightMissionId(),
+                            a.getFlightMissionId() != 0 ? FlightMissionHelper.formatRouteOrNull(world, a.getFlightMissionId()) : null,
+                            a.getOperationalStatus().name(),
+                            a.getLocationStatus().name(),
+                            a.getLocationAirportId() != 0 ? world.airports().byId(a.getLocationAirportId()).orElseThrow().getIcao() : null,
+                            a.getLocationLatitude(),
+                            a.getLocationLongitude()))
+                    .toList());
+        }
     }
 
     @GetMapping("/flying")
     public List<FlyingAircraftDto> getFlying() {
-        return worldBean.read(world ->  world.aircrafts().all().stream()
-                .filter(a -> a.getLocationStatus() == Aircrafts.LocationStatus.Flying)
-                .map(a -> {
-                    final FlightMissions.Mission mission = world.flightMissions().byId(a.getFlightMissionId()).orElseThrow();
-                    return new FlyingAircraftDto(
-                            a.getId(),
-                            world.aircraftTypes().byId(a.getAircraftTypeId()).orElseThrow().getIcao(),
-                            a.getRegNo(),
-                            a.getLocationLatitude(),
-                            a.getLocationLongitude(),
-                            (int) FlightMissionHelper.calculateHeading(world, a.getFlightMissionId()),
-                            world.airports().byId(mission.getDepartureAirportId()).orElseThrow().getIcao(),
-                            world.airports().byId(mission.getDestinationAirportId()).orElseThrow().getIcao(),
-                            WebTime.hhmmOrNull(mission.getPlannedDepartureWorldTime()),
-                            WebTime.hhmmOrNull(mission.getPlannedArrivalWorldTime()),
-                            WebTime.hhmmOrNull(mission.getActualTakeoffWorldTime()),
-                            null);
-                })
-                .toList());
+        try (final Timing.Timer ignored = Timing.label("AircraftController - getFlying")) {
+            return worldBean.read(world -> world.aircrafts().all().stream()
+                    .filter(a -> a.getLocationStatus() == Aircrafts.LocationStatus.Flying)
+                    .map(a -> {
+                        final FlightMissions.Mission mission = world.flightMissions().byId(a.getFlightMissionId()).orElseThrow();
+                        return new FlyingAircraftDto(
+                                a.getId(),
+                                world.aircraftTypes().byId(a.getAircraftTypeId()).orElseThrow().getIcao(),
+                                a.getRegNo(),
+                                a.getLocationLatitude(),
+                                a.getLocationLongitude(),
+                                (int) FlightMissionHelper.calculateHeading(world, a.getFlightMissionId()),
+                                world.airports().byId(mission.getDepartureAirportId()).orElseThrow().getIcao(),
+                                world.airports().byId(mission.getDestinationAirportId()).orElseThrow().getIcao(),
+                                WebTime.hhmmOrNull(mission.getPlannedDepartureWorldTime()),
+                                WebTime.hhmmOrNull(mission.getPlannedArrivalWorldTime()),
+                                WebTime.hhmmOrNull(mission.getActualTakeoffWorldTime()),
+                                null);
+                    })
+                    .toList());
+        }
     }
 
     @Data
--- src/main/java/net/simforge/airways2/app/GeoController.java
+++ src/main/java/net/simforge/airways2/app/GeoController.java
@@ -2,6 +2,7 @@ package net.simforge.airways2.app;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import net.simforge.airways2.app.tools.Timing;
 import net.simforge.airways2.world.datamodel.Aircrafts;
 import net.simforge.airways2.world.datamodel.Airports;
 import net.simforge.airways2.world.datamodel.Cities;
@@ -24,93 +25,101 @@ public class GeoController {
 
     @GetMapping("/countries")
     public List<CountryDto> getCountries() {
-        return worldBean.read(world -> world.countries().all().stream()
-                .map(c -> new CountryDto(
-                        c.getId(),
-                        c.getCode(),
-                        c.getName()))
-                .toList());
+        try (final Timing.Timer ignored = Timing.label("GeoController - getCountries")) {
+            return worldBean.read(world -> world.countries().all().stream()
+                    .map(c -> new CountryDto(
+                            c.getId(),
+                            c.getCode(),
+                            c.getName()))
+                    .toList());
+        }
     }
 
     @GetMapping("/cities")
     public List<CityDto> getCities() {
-        return worldBean.read(world -> world.cities().all().stream()
-                .map(c -> new CityDto(
-                        c.getId(),
-                        c.getCountryId(),
-                        c.getName(),
-                        c.getPopulation(),
-                        c.getLatitude(),
-                        c.getLongitude()))
-                .toList());
+        try (final Timing.Timer ignored = Timing.label("GeoController - getCities")) {
+            return worldBean.read(world -> world.cities().all().stream()
+                    .map(c -> new CityDto(
+                            c.getId(),
+                            c.getCountryId(),
+                            c.getName(),
+                            c.getPopulation(),
+                            c.getLatitude(),
+                            c.getLongitude()))
+                    .toList());
+        }
     }
 
     @GetMapping("/airports")
     public List<AirportDto> getAirports() {
-        return worldBean.read(world -> world.airports().all().stream()
-                .map(a -> new AirportDto(
-                        a.getId(),
-                        a.getLatitude(),
-                        a.getLongitude(),
-                        a.getIata(),
-                        a.getIcao(),
-                        a.getName()))
-                .toList());
+        try (final Timing.Timer ignored = Timing.label("GeoController - getAirports")) {
+            return worldBean.read(world -> world.airports().all().stream()
+                    .map(a -> new AirportDto(
+                            a.getId(),
+                            a.getLatitude(),
+                            a.getLongitude(),
+                            a.getIata(),
+                            a.getIcao(),
+                            a.getName()))
+                    .toList());
+        }
     }
 
     @GetMapping("/airport/{icao}/details")
     public AirportDetailsDto getAirportDetails(@PathVariable final String icao) {
-        return worldBean.read(world -> {
-            final Airports.Airport airport = world.airports().byIcao(icao).orElseThrow();
-            final List<String> connectedCities = world.airport2city().allByAirportId(airport.getId()).stream()
-                    .map(l -> world.cities().byId(l.getCityId()).orElseThrow())
-                    .sorted((c1, c2) -> c2.getPopulation() - c1.getPopulation())
-                    .map(Cities.City::getName)
-                    .toList();
-            final Map<String, IcaoToFlights> outboundConnections = world.airport2airportDailyFlightStats()
-                    .allByFromAirportId(airport.getId()).stream()
-                    .map(fs -> new IcaoToFlights(world.airports().getIcao(fs.getToAirportId()), fs.getTotalCount()))
-                    .collect(Collectors.toMap(p -> p.icao, p -> p));
-            final Map<String, IcaoToFlights> inboundConnections = world.airport2airportDailyFlightStats()
-                    .allByToAirportId(airport.getId()).stream()
-                    .map(fs -> new IcaoToFlights(world.airports().getIcao(fs.getFromAirportId()), fs.getTotalCount()))
-                    .collect(Collectors.toMap(p -> p.icao, p -> p));
-            final Map<String, IcaoToFlights> totalConnections = Stream.concat(
-                    outboundConnections.entrySet().stream(),
-                    inboundConnections.entrySet().stream()
-            ).collect(Collectors.toMap(
-                    Map.Entry::getKey,
-                    Map.Entry::getValue,
-                    IcaoToFlights::merge
-            ));
-            final List<IcaoToFlights> top3connections = totalConnections.values().stream()
-                    .sorted(Comparator.comparingInt(IcaoToFlights::getFlights).reversed())
-                    .limit(3)
-                    .toList();
-            final int flightsOutbound = (int) world.flightMissions().all().stream()
-                    .filter(f -> f.getDepartureAirportId() == airport.getId() && isFlightAlive(f.getStatus()))
-                    .count();
-            final int flightsInbound = (int) world.flightMissions().all().stream()
-                    .filter(f -> f.getDestinationAirportId() == airport.getId() && isFlightAlive(f.getStatus()))
-                    .count();
-            final int aircraftParked = (int) world.aircrafts().all().stream()
-                    .filter(a -> a.getLocationStatus() == Aircrafts.LocationStatus.ParkedAtAirport
-                            && a.getLocationAirportId() == airport.getId()
-                            && a.getOperationalStatus() == Aircrafts.OperationalStatus.Idle)
-                    .count();
-            final int aircraftActive = (int) world.aircrafts().all().stream()
-                    .filter(a -> a.getLocationAirportId() == airport.getId()
-                            && a.getOperationalStatus() == Aircrafts.OperationalStatus.Active)
-                    .count();
-            return new AirportDetailsDto(
-                    connectedCities,
-                    top3connections,
-                    flightsOutbound,
-                    flightsInbound,
-                    aircraftParked,
-                    aircraftActive
-            );
-        });
+        try (final Timing.Timer ignored = Timing.label("GeoController - getAirportDetails")) {
+            return worldBean.read(world -> {
+                final Airports.Airport airport = world.airports().byIcao(icao).orElseThrow();
+                final List<String> connectedCities = world.airport2city().allByAirportId(airport.getId()).stream()
+                        .map(l -> world.cities().byId(l.getCityId()).orElseThrow())
+                        .sorted((c1, c2) -> c2.getPopulation() - c1.getPopulation())
+                        .map(Cities.City::getName)
+                        .toList();
+                final Map<String, IcaoToFlights> outboundConnections = world.airport2airportDailyFlightStats()
+                        .allByFromAirportId(airport.getId()).stream()
+                        .map(fs -> new IcaoToFlights(world.airports().getIcao(fs.getToAirportId()), fs.getTotalCount()))
+                        .collect(Collectors.toMap(p -> p.icao, p -> p));
+                final Map<String, IcaoToFlights> inboundConnections = world.airport2airportDailyFlightStats()
+                        .allByToAirportId(airport.getId()).stream()
+                        .map(fs -> new IcaoToFlights(world.airports().getIcao(fs.getFromAirportId()), fs.getTotalCount()))
+                        .collect(Collectors.toMap(p -> p.icao, p -> p));
+                final Map<String, IcaoToFlights> totalConnections = Stream.concat(
+                        outboundConnections.entrySet().stream(),
+                        inboundConnections.entrySet().stream()
+                ).collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        Map.Entry::getValue,
+                        IcaoToFlights::merge
+                ));
+                final List<IcaoToFlights> top3connections = totalConnections.values().stream()
+                        .sorted(Comparator.comparingInt(IcaoToFlights::getFlights).reversed())
+                        .limit(3)
+                        .toList();
+                final int flightsOutbound = (int) world.flightMissions().all().stream()
+                        .filter(f -> f.getDepartureAirportId() == airport.getId() && isFlightAlive(f.getStatus()))
+                        .count();
+                final int flightsInbound = (int) world.flightMissions().all().stream()
+                        .filter(f -> f.getDestinationAirportId() == airport.getId() && isFlightAlive(f.getStatus()))
+                        .count();
+                final int aircraftParked = (int) world.aircrafts().all().stream()
+                        .filter(a -> a.getLocationStatus() == Aircrafts.LocationStatus.ParkedAtAirport
+                                && a.getLocationAirportId() == airport.getId()
+                                && a.getOperationalStatus() == Aircrafts.OperationalStatus.Idle)
+                        .count();
+                final int aircraftActive = (int) world.aircrafts().all().stream()
+                        .filter(a -> a.getLocationAirportId() == airport.getId()
+                                && a.getOperationalStatus() == Aircrafts.OperationalStatus.Active)
+                        .count();
+                return new AirportDetailsDto(
+                        connectedCities,
+                        top3connections,
+                        flightsOutbound,
+                        flightsInbound,
+                        aircraftParked,
+                        aircraftActive
+                );
+            });
+        }
     }
 
     private boolean isFlightAlive(final FlightMissions.Status status) {
