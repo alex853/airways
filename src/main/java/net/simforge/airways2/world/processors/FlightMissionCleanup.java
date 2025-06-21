252c20539c5c82c5ade2324ae38d41258014a406
alex853 <alexxxey.kornev@gmail.com>
2025-06-21 15:28:35 +0300

    flight missions cleanup - finished status

--- src/main/java/net/simforge/airways2/world/processors/FlightMissionCleanup.java
+++ src/main/java/net/simforge/airways2/world/processors/FlightMissionCleanup.java
@@ -6,7 +6,6 @@ import net.simforge.airways2.world.datamodel.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
 import java.util.Collection;
 
 public class FlightMissionCleanup {
@@ -14,11 +13,17 @@ public class FlightMissionCleanup {
 
     public static void process(final World world) {
         final int worldTime = world.getWorldTime();
+
         final Collection<FlightMissions.Mission> cancelled = world.flightMissions()
                 .filter(f -> f.getStatus() == FlightMissions.Status.Cancelled
                         && f.getPlannedDepartureWorldTime() <= worldTime - 2 * Time.ONE_DAY);
         cancelled.forEach(f -> world.flightMissions().deleteById(f.getId()));
-        final Collection<FlightMissions.Mission> finished = new ArrayList<>();
+
+        final Collection<FlightMissions.Mission> finished = world.flightMissions()
+                .filter(f -> f.getStatus() == FlightMissions.Status.Finished
+                        && f.getActualArrivalWorldTime() <= worldTime - 14 * Time.ONE_DAY);
+        finished.forEach(f -> world.flightMissions().deleteById(f.getId()));
+
         if (cancelled.size() > 0 || finished.size() > 0) {
             log.info("flight missions cleaned up - {} cancelled, {} finished", cancelled.size(), finished.size());
         }
