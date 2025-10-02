72be88942617cf3fc632ea34e80d4dcdf99c63f7
Alexey Kornev <alexxxey.kornev@gmail.com>
2025-10-02 17:30:14 +0300

    Update EventLogController.java

--- src/main/java/net/simforge/airways2/app/EventLogController.java
+++ src/main/java/net/simforge/airways2/app/EventLogController.java
@@ -2,10 +2,12 @@ package net.simforge.airways2.app;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import net.simforge.airways2.world.datamodel.EventLog;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
