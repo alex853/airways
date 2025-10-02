09a875f3e7501d121b3c70532d61d3494f121cb9
Alexey Kornev <alexxxey.kornev@gmail.com>
2025-10-02 14:19:56 +0300

    Update TransportFlightHelper.java

--- src/main/java/net/simforge/airways2/world/processors/TransportFlightHelper.java
+++ src/main/java/net/simforge/airways2/world/processors/TransportFlightHelper.java
@@ -5,10 +5,15 @@ import net.simforge.airways2.world.datamodel.FlightMissions;
 
 public class TransportFlightHelper {
     private static final int CHECKIN_STARTS_BEFORE = 120 * Time.ONE_MINUTE;
+    private static final int CHECKIN_ENDS_BEFORE = 30 * Time.ONE_MINUTE;
 
     public static int calcCheckinStartTime(final FlightMissions.Mission flightMission) {
+        checkNotNull(flightMission);
         return flightMission.getPlannedDepartureWorldTime() - CHECKIN_STARTS_BEFORE;
     }
 
-    // todo ak0 calcCheckinEndTime - 30 mins before planned departure time
+    public static int calcCheckinEndTime(final FlightMissions.Mission flightMission) {
+        checkNotNull(flightMission);
+        return flightMission.getPlannedDepartureWorldTime() - CHECKIN_ENDS_BEFORE;
+    }
 }
