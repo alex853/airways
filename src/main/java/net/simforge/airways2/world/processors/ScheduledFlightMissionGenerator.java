1071c5a3a7d6eff919e0b04d2d3e8cace7c89193
Alexey Kornev <alexxxey.kornev@gmail.com>
2025-10-06 15:00:34 +0300

    Update ScheduledFlightMissionGenerator.java

--- src/main/java/net/simforge/airways2/world/processors/ScheduledFlightMissionGenerator.java
+++ src/main/java/net/simforge/airways2/world/processors/ScheduledFlightMissionGenerator.java
@@ -30,22 +30,28 @@ public class ScheduledFlightMissionGenerator {
             new ScheduledFlight(122, "AW122", "F-AUWA", "LIRF", "LFPG", "15:00"),
             new ScheduledFlight(103, "AW103", "F-AUWA", "LFPG", "EGLL", "19:00"),
             new ScheduledFlight(104, "AW104", "F-AUWA", "EGLL", "LFPG", "22:00"),
+        
             new ScheduledFlight(131, "AW131", "F-AUWB", "LFPG", "EDDM", "06:00"),
             new ScheduledFlight(132, "AW132", "F-AUWB", "EDDM", "LFPG", "10:00"),
             new ScheduledFlight(141, "AW141", "F-AUWB", "LFPG", "LEBL", "14:00"),
             new ScheduledFlight(142, "AW142", "F-AUWB", "LEBL", "LFPG", "18:00"),
+            new ScheduledFlight(105, "AW105", "F-AUWB", "LFPG", "EGLL", "22:00"),
+            new ScheduledFlight(106, "AW106", "F-AUWB", "EGLL", "LFPG", "02:00"),
+        
             new ScheduledFlight(151, "AW151", "F-AUWC", "LFPG", "EDDH", "04:00"),
             new ScheduledFlight(152, "AW152", "F-AUWC", "EDDH", "LFPG", "07:00"),
             new ScheduledFlight(153, "AW153", "F-AUWC", "LFPG", "EDDB", "10:00"),
             new ScheduledFlight(154, "AW154", "F-AUWC", "EDDB", "LFPG", "13:00"),
             new ScheduledFlight(155, "AW155", "F-AUWC", "LFPG", "LKPR", "16:00"),
             new ScheduledFlight(156, "AW156", "F-AUWC", "LKPR", "LFPG", "19:00"),
+        
             new ScheduledFlight(161, "AW161", "F-AUWD", "LFPG", "LOWW", "04:00"),
             new ScheduledFlight(162, "AW162", "F-AUWD", "LOWW", "LFPG", "07:00"),
             new ScheduledFlight(163, "AW163", "F-AUWD", "LFPG", "EIDW", "10:00"),
             new ScheduledFlight(164, "AW164", "F-AUWD", "EIDW", "LFPG", "13:00"),
             new ScheduledFlight(165, "AW165", "F-AUWD", "LFPG", "EGPH", "16:00"),
             new ScheduledFlight(166, "AW166", "F-AUWD", "EGPH", "LFPG", "19:00"),
+        
             new ScheduledFlight(171, "AW171", "F-AUWE", "LFPG", "LIMC", "04:30"),
             new ScheduledFlight(172, "AW172", "F-AUWE", "LIMC", "LFPG", "08:00"),
             new ScheduledFlight(173, "AW173", "F-AUWE", "LFPG", "LEMD", "11:30"),
