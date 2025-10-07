8be4f04fa87815803c3ebef1c11ae0cd18770352
alex853 <alexxxey.kornev@gmail.com>
2025-10-07 20:34:40 +0300

    flight-dashboard shown elements

--- src/main/java/net/simforge/airways2/app/FlightDashboardController.java
+++ src/main/java/net/simforge/airways2/app/FlightDashboardController.java
@@ -56,7 +56,7 @@ public class FlightDashboardController {
                     flightId,
                     flight.getStatus().name(),
                     getNextPlannedFlightMissionStatus(flight),
-                    getFlightMissionPermittedActions(flight, transportFlight, world),
+                    getFlightMissionShownElements(flight, transportFlight, world),
                     world.airports().getIcao(flight.getDepartureAirportId()),
                     world.airports().getIcao(flight.getDestinationAirportId()),
                     WebTime.hhmmOrNull(flight.getPlannedDepartureWorldTime()),
@@ -67,7 +67,7 @@ public class FlightDashboardController {
                     transportFlight.getId(),
                     transportFlight.getStatus().name(),
                     getNextPlannedTransportFlightStatus(transportFlight, flight),
-                    getTransportFlightPermittedActions(transportFlight, flight),
+                    getTransportFlightShownElements(transportFlight, flight),
                     transportFlight.getTotalTickets().toString(),
                     transportFlight.getRemainedTickets().toString(),
                     null, // todo ak0 substraction
@@ -92,7 +92,7 @@ public class FlightDashboardController {
         };
     }
 
-    private String getFlightMissionPermittedActions(final FlightMissions.Mission flight, final TransportFlights.Flight transportFlight, final World world) {
+    private String getFlightMissionShownElements(final FlightMissions.Mission flight, final TransportFlights.Flight transportFlight, final World world) {
         return switch (flight.getStatus()) {
             case Dispatched -> (FlightMissionHelper.calcPreflightStartTime(flight) <= world.getWorldTime()) ? "start" : "start-early-with-caution";
             case Preflight -> (transportFlight == null || transportFlight.getStatus() == WaitingForDeparture) ? "blocks-off" : "blocks-off-disabled";
@@ -120,11 +120,12 @@ public class FlightDashboardController {
         };
     }
 
-    private String getTransportFlightPermittedActions(final TransportFlights.Flight transportFlight, final FlightMissions.Mission flight) {
+    private String getTransportFlightShownElements(final TransportFlights.Flight transportFlight, final FlightMissions.Mission flight) {
         return switch (transportFlight.getStatus()) {
             case CheckIn -> "start-boarding-disabled";
             case WaitingForBoarding -> flight.getStatus() == Preflight ? "start-boarding" : "start-boarding-disabled";
-            case Arrival -> "start-deboarding-disabled";
+            case WaitingForDeparture, Departure, Flying -> "on-board";
+            case Arrival -> "start-deboarding-disabled,on-board";
             case WaitingForDeboarding -> "start-deboarding";
             default -> null;
         };
@@ -139,7 +140,7 @@ public class FlightDashboardController {
             checkArgument(flight.getStatus() == FlightMissions.Status.Dispatched, "flight status is not as expected");
 
             final TransportFlights.Flight transportFlight = world.transportFlights().byFlightMissionId(flightId).orElse(null);
-            final String permitted = getFlightMissionPermittedActions(flight, transportFlight, world);
+            final String permitted = getFlightMissionShownElements(flight, transportFlight, world);
             if (!"start".equals(permitted)) {
                 throw new IllegalStateException("start is not permitted");
             }
@@ -162,7 +163,7 @@ public class FlightDashboardController {
             checkArgument(flight.getStatus() == FlightMissions.Status.Preflight, "flight status is not as expected");
             checkArgument(transportFlight.getStatus() == TransportFlights.Status.WaitingForBoarding, "transport flight status is not as expected");
 
-            final String permitted = getTransportFlightPermittedActions(transportFlight, flight);
+            final String permitted = getTransportFlightShownElements(transportFlight, flight);
             if (!"start-boarding".equals(permitted)) {
                 throw new IllegalStateException("start-boarding is not permitted");
             }
@@ -181,7 +182,7 @@ public class FlightDashboardController {
             checkArgument(flight.isModePc(), "flight should be in manual mode");
             checkArgument(flight.getStatus() == Preflight, "flight status is not as expected");
 
-            final String permitted = getFlightMissionPermittedActions(flight, transportFlight, world);
+            final String permitted = getFlightMissionShownElements(flight, transportFlight, world);
             if (!"blocks-off".equals(permitted)) {
                 throw new IllegalStateException("blocks-off is not permitted");
             }
@@ -201,7 +202,7 @@ public class FlightDashboardController {
             checkArgument(flight.isModePc(), "flight should be in manual mode");
             checkArgument(flight.getStatus() == FlightMissions.Status.Departure, "flight status is not as expected");
 
-            final String permitted = getFlightMissionPermittedActions(flight, transportFlight, world);
+            final String permitted = getFlightMissionShownElements(flight, transportFlight, world);
             if (!"takeoff".equals(permitted)) {
                 throw new IllegalStateException("takeoff is not permitted");
             }
@@ -221,7 +222,7 @@ public class FlightDashboardController {
             checkArgument(flight.isModePc(), "flight should be in manual mode");
             checkArgument(flight.getStatus() == FlightMissions.Status.Flying, "flight status is not as expected");
 
-            final String permitted = getFlightMissionPermittedActions(flight, transportFlight, world);
+            final String permitted = getFlightMissionShownElements(flight, transportFlight, world);
             if (!"landing".equals(permitted)) {
                 throw new IllegalStateException("landing is not permitted");
             }
@@ -242,7 +243,7 @@ public class FlightDashboardController {
             checkArgument(flight.isModePc(), "flight should be in manual mode");
             checkArgument(flight.getStatus() == FlightMissions.Status.Arrival, "flight status is not as expected");
 
-            final String permitted = getFlightMissionPermittedActions(flight, transportFlight, world);
+            final String permitted = getFlightMissionShownElements(flight, transportFlight, world);
             if (!"blocks-on".equals(permitted)) {
                 throw new IllegalStateException("blocks-on is not permitted");
             }
@@ -265,7 +266,7 @@ public class FlightDashboardController {
             checkArgument(flight.getStatus() == Postflight, "flight status is not as expected");
             checkArgument(transportFlight.getStatus() == TransportFlights.Status.WaitingForDeboarding, "transport flight status is not as expected");
 
-            final String permitted = getTransportFlightPermittedActions(transportFlight, flight);
+            final String permitted = getTransportFlightShownElements(transportFlight, flight);
             if (!"start-deboarding".equals(permitted)) {
                 throw new IllegalStateException("start-deboarding is not permitted");
             }
@@ -285,7 +286,7 @@ public class FlightDashboardController {
             checkArgument(flight.isModePc(), "flight should be in manual mode");
             checkArgument(flight.getStatus() == FlightMissions.Status.Postflight, "flight status is not as expected");
 
-            final String permitted = getFlightMissionPermittedActions(flight, transportFlight, world);
+            final String permitted = getFlightMissionShownElements(flight, transportFlight, world);
             if (!"finish".equals(permitted)) {
                 throw new IllegalStateException("finish is not permitted");
             }
@@ -320,7 +321,7 @@ public class FlightDashboardController {
         private int id;
         private String status;
         private String nextPlannedStatus;
-        private String permittedActions;
+        private String shownElements;
         private String depIcao;
         private String destIcao;
         private String planDepTime;
@@ -333,7 +334,7 @@ public class FlightDashboardController {
         private int id;
         private String status;
         private String nextPlannedStatus;
-        private String permittedActions;
+        private String shownElements;
         private String totalTickets;
         private String remainedTickets;
         private String soldTickets;
