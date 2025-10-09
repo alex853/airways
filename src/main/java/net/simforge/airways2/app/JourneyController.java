6937849740a33a43ee80bba935e58de11a49b1b0
Alexey Kornev <alexxxey.kornev@gmail.com>
2025-10-09 16:19:41 +0300

    Update JourneyController.java

--- src/main/java/net/simforge/airways2/app/JourneyController.java
+++ src/main/java/net/simforge/airways2/app/JourneyController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
