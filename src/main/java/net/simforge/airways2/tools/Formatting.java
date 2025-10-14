7213900d86d062025f4a50a02952b11aba39b8ea
Alexey Kornev <alexxxey.kornev@gmail.com>
2025-10-14 19:12:26 +0300

    Update Formatting.java

--- src/main/java/net/simforge/airways2/tools/Formatting.java
+++ src/main/java/net/simforge/airways2/tools/Formatting.java
@@ -4,4 +4,7 @@ import java.text.DecimalFormat;
 
 public class Formatting {
     public static final DecimalFormat df3 = new DecimalFormat("#.###");
+    public static final DecimalFormat df5z = new DecimalFormat("#.00000");
+    public static final DecimalFormat df6z = new DecimalFormat("#.000000");
+    public static final DecimalFormat df7z = new DecimalFormat("#.0000000");
 }
