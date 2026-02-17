277e803eddf03eed73b3dd8faff0b493512793f0
alex853 <alexxxey.kornev@gmail.com>
2026-02-17 20:10:25 +0200

    adding exception message

--- src/main/java/net/simforge/airways2/world/processors/JourneyProcessor.java
+++ src/main/java/net/simforge/airways2/world/processors/JourneyProcessor.java
@@ -140,7 +140,7 @@ public class JourneyProcessor {
     private static TFM toTfm(final World world, final TransportFlights.Flight tf) {
         return new TFM(tf, world.flightMissions()
                 .byId(tf.getFlightMissionId())
-                .orElseThrow(() -> new IllegalStateException("Unable to find F/M # " + tf.getFlightMissionId() + " for T/F " + tf)));
+                .orElseThrow(() -> new NoSuchElementException("Unable to find F/M # " + tf.getFlightMissionId() + " for T/F " + tf)));
     }
 
     private static boolean isThereEnoughTickets(final Journeys.Journey journey, final TransportFlights.Flight tf) {
