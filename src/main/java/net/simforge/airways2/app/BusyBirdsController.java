9cf8d7027a8eafe0b73332909d32ab30702c4d37
alex853 <alexxxey.kornev@gmail.com>
2026-04-05 16:54:08 +0300

    busybirds - web controller

--- src/main/java/net/simforge/airways2/app/BusyBirdsController.java
+++ src/main/java/net/simforge/airways2/app/BusyBirdsController.java
@@ -3,6 +3,7 @@ package net.simforge.airways2.app;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import net.simforge.airways2.app.tools.Timing;
+import net.simforge.airways2.world.datamodel.Aircrafts;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,17 +19,33 @@ public class BusyBirdsController {
     @Autowired
     private WorldRunnerBean worldBean;
 
-    @GetMapping("/mission/all")
-    public List<MissionDto> getMissions() {
-        try (final Timing.Timer ignored = Timing.label("BusyBirdsController - getAllMissions")) {
+    @GetMapping("/mission/to-book")
+    public List<MissionDto> getMissionsToBook() {
+        try (final Timing.Timer ignored = Timing.label("BusyBirdsController - getMissionsToBook")) {
             return worldBean.read(world -> world.journeys().filter(world.journeys().bySpecialProcessing())
-                    .map(j -> new MissionDto(
+                    .map(j -> new MissionDto( // todo ak0 some filtering by status
                             j.getId(),
                             j.getFromCityId(),
                             world.cities().byId(j.getFromCityId()).get().getName(),
                             j.getToCityId(),
                             world.cities().byId(j.getToCityId()).get().getName(),
                             j.getGroupSize()))
+                    .toList());
+        }
+    }
+
+    @GetMapping("/aircraft/available")
+    public List<AircraftDto> getAvailableAircraft() {
+        try (final Timing.Timer ignored = Timing.label("BusyBirdsController - getAvailableAircraft")) {
+            return worldBean.read(world -> world.aircrafts()
+                    .byAircraftOperatorId(3) // todo ak3 this operatorId should go into some constant
+                    .filter(Aircrafts::isIdleAndParkedAtAirport)
+                    .map(a -> new AircraftDto(
+                            a.getId(),
+                            world.aircraftTypes().byId(a.getAircraftTypeId()).get().getIcao(),
+                            a.getRegNo(),
+                            a.getLocationAirportId(),
+                            world.airports().byId(a.getLocationAirportId()).get().getIcao()))
                     .toList());
         }
     }
@@ -42,5 +59,15 @@ public class BusyBirdsController {
         private int toCityId;
         private String toCityName;
         private int pax;
+    }
+
+    @Data
+    @AllArgsConstructor
+    private static class AircraftDto {
+        private int id;
+        private String typeCode;
+        private String regNo;
+        private int locationAirportId;
+        private String locationAirportIcao;
     }
 }
