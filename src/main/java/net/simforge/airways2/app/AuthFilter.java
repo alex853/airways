a48cac59153d9f959503e2d88d6159ce07a5e1d4
Alexey Kornev <alexey.kornev@jetbrains.com>
2026-04-08 18:21:18 +0300

    flight-dashboard - cross-origin fight

--- src/main/java/net/simforge/airways2/app/AuthFilter.java
+++ src/main/java/net/simforge/airways2/app/AuthFilter.java
@@ -18,6 +18,11 @@ public class AuthFilter extends OncePerRequestFilter {
                                     FilterChain filterChain)
             throws ServletException, IOException {
 
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
         String uri = request.getRequestURI();
         if (uri.startsWith("/busy-birds")
             || uri.startsWith("/flight-dashboard")) {
