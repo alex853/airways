3ddd83ef47506936fbe1ebefec6da16bf2ea8f2b
Alexey Kornev <alexey.kornev@jetbrains.com>
2026-04-08 18:00:10 +0300

    flight-dashboard - cross-origin fight

--- src/main/java/net/simforge/airways2/app/user/FlightDashboardController.java
+++ src/main/java/net/simforge/airways2/app/user/FlightDashboardController.java
@@ -30,7 +30,7 @@ import static net.simforge.airways2.world.datamodel.TransportFlights.Status.Flyi
 
 @RestController
 @RequestMapping("/flight-dashboard")
-@CrossOrigin
+@CrossOrigin(allowedHeaders = "*", origins = "*")
 public class FlightDashboardController {
     // todo ak1 migrate ids to sqids
     private static final Logger log = LoggerFactory.getLogger(FlightDashboardController.class);
