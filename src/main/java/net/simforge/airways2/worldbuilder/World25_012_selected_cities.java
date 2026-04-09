16fd3d5cf12b53f2f9c09b0ce3649d102de0536b
alex853 <alexxxey.kornev@gmail.com>
2026-04-09 23:20:31 +0300

    World25 - Frankfurt added

--- src/main/java/net/simforge/airways2/worldbuilder/World25_012_selected_cities.java
+++ src/main/java/net/simforge/airways2/worldbuilder/World25_012_selected_cities.java
@@ -7,6 +7,8 @@ import java.io.IOException;
 
 public class World25_012_selected_cities {
     public static void main(String[] args) throws IOException {
+        ImportCities.main(new String[]{"country-code:DE", "city-name:Frankfurt am Main"});
+
         ImportCities.main(new String[]{"country-code:GB", "city-name:Liverpool"});
         ImportCities.main(new String[]{"country-code:GB", "city-name:Edinburgh"});
         ImportCities.main(new String[]{"country-code:GB", "city-name:Manchester"});
