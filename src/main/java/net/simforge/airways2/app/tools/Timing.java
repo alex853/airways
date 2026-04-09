8ea8c503bb8d6cae72ece96a9076adc6a89fc6c6
Alexey Kornev <alexey.kornev@jetbrains.com>
2026-04-09 17:47:49 +0300

    FlightMissions - heartbeatTime index introduced

--- src/main/java/net/simforge/airways2/app/tools/Timing.java
+++ src/main/java/net/simforge/airways2/app/tools/Timing.java
@@ -11,7 +11,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class Timing {
     private static final Logger log = LoggerFactory.getLogger(Timing.class);
     private static final ConcurrentHashMap<String, LabelData> data = new ConcurrentHashMap<>();
-    private static final int PRINT_STATUS_EVERY_N_MEASURES = 100000;
+    private static final int PRINT_STATUS_EVERY_N_MEASURES = 100_000_000;
     private static final AtomicInteger counterToStatusPrinting = new AtomicInteger(PRINT_STATUS_EVERY_N_MEASURES);
 
     public static Timer label(final String label) {
