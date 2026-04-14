d5b7994adc384ed600589938ec231e7aa43ae522
Alexey Kornev <alexey.kornev@jetbrains.com>
2026-04-14 12:45:47 +0300

    World25 - some german cities added

--- src/main/java/net/simforge/airways2/world/processors/CityFlowHelper.java
+++ src/main/java/net/simforge/airways2/world/processors/CityFlowHelper.java
@@ -25,7 +25,7 @@ public class CityFlowHelper {
     public static final float DEFAULT_MOBILITY_FACTOR = 1.0f;
 
     private static final float MIN_SUCCESS_RATE = 0.00001f;
-    public static final float STARTING_SUCCESS_RATE = 0.01f;
+    public static final float STARTING_SUCCESS_RATE = 0.10f;
     private static final float MAX_SUCCESS_RATE = 1.0f;
 
     public static float getFlowUnits(final World world, final CityFlows.Flow fromCityFlow, final CityFlows.Flow toCityFlow) {
