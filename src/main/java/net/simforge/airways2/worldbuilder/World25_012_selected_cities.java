87b030b621e2943cc108223b6abe0be0d20db0aa
Alexey Kornev <alexey.kornev@jetbrains.com>
2026-04-15 16:17:22 +0300

    World25 - daily updates

--- src/main/java/net/simforge/airways2/worldbuilder/World25_012_selected_cities.java
+++ src/main/java/net/simforge/airways2/worldbuilder/World25_012_selected_cities.java
@@ -7,6 +7,8 @@ import java.io.IOException;
 
 public class World25_012_selected_cities {
     public static void main(String[] args) throws IOException {
+        ImportCities.main(new String[]{"country-code:CA", "city-name:Toronto"});
+
         ImportCities.main(new String[]{"country-code:DE", "city-name:Frankfurt am Main"});
         ImportCities.main(new String[]{"country-code:DE", "city-name:Bremen"});
         ImportCities.main(new String[]{"country-code:DE", "city-name:Hannover"});
@@ -24,6 +26,11 @@ public class World25_012_selected_cities {
         ImportCities.main(new String[]{"country-code:GB", "city-name:Liverpool"});
         ImportCities.main(new String[]{"country-code:GB", "city-name:Edinburgh"});
         ImportCities.main(new String[]{"country-code:GB", "city-name:Manchester"});
+
+        ImportCities.main(new String[]{"country-code:US", "city-name:Boston"});
+        ImportCities.main(new String[]{"country-code:US", "city-name:New York"});
+        ImportCities.main(new String[]{"country-code:US", "city-name:Austin"});
+        ImportCities.main(new String[]{"country-code:US", "city-name:Los Angeles"});
 
         ImportMajorAirportsInVicinityOfCities.main(new String[0]);
     }
