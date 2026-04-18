2628e6e54c7c9bfeac8bbb921f484cb587335f92
Alexey Kornev <alexey.kornev@jetbrains.com>
2026-04-18 22:45:15 +0300

    World25 - daily updates

--- src/main/java/net/simforge/airways2/worldbuilder/World25_012_selected_cities.java
+++ src/main/java/net/simforge/airways2/worldbuilder/World25_012_selected_cities.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 public class World25_012_selected_cities {
     public static void main(String[] args) throws IOException {
         ImportCities.main(new String[]{"country-code:CA", "city-name:Toronto"});
+        ImportCities.main(new String[]{"country-code:CA", "city-name:Montreal"});
 
         ImportCities.main(new String[]{"country-code:DE", "city-name:Frankfurt am Main"});
         ImportCities.main(new String[]{"country-code:DE", "city-name:Bremen"});
