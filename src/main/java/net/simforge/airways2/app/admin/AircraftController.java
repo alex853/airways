43c241adfc3b60f5706eb0d32ef3511c2c6eef60
alex853 <alexxxey.kornev@gmail.com>
2026-05-21 15:52:35 +0300

    aircraft refactoring

--- src/main/java/net/simforge/airways2/app/admin/AircraftController.java
+++ src/main/java/net/simforge/airways2/app/admin/AircraftController.java
@@ -3,10 +3,7 @@ package net.simforge.airways2.app.admin;
 import net.simforge.airways2.app.beans.WorldRunnerBean;
 import net.simforge.airways2.app.dto.AircraftFullDto;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,8 +15,11 @@ public class AircraftController {
     private WorldRunnerBean worldBean;
 
     @GetMapping("/all")
-    public List<AircraftFullDto> getAll() {
+    public List<AircraftFullDto> getAll(@RequestParam(name = "offset", required = false) final Integer offset,
+                                        @RequestParam(name = "limit", required = false) final Integer limit) {
         return worldBean.read(world -> world.aircrafts().all()
+                .skip(offset != null ? offset : 0)
+                .limit(limit != null ? limit : Long.MAX_VALUE)
                 .map(a -> AircraftFullDto.from(world, a))
                 .toList());
     }
