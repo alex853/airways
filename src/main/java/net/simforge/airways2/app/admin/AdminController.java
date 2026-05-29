ad4e3578d1825dd7081216e1e2048bafae000f81
alex853 <alexxxey.kornev@gmail.com>
2026-05-29 12:47:48 +0300

    incident fixing - list of frozen aircraft

--- src/main/java/net/simforge/airways2/app/admin/AdminController.java
+++ src/main/java/net/simforge/airways2/app/admin/AdminController.java
@@ -35,6 +35,7 @@ import java.lang.reflect.Type;
 import java.time.LocalDate;
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
@@ -389,6 +390,28 @@ public class AdminController {
             AircraftHelper.moveParkedAircraftToAnotherAirport(world, aircraft, airport);
 
             return "A/C #" + aircraft.getId() + " is parked in airport #" + aircraft.getLocationAirportId();
+        });
+    }
+
+    @GetMapping("/aircraft/frozen-lsit")
+    public String resetAircraftStatus() {
+        return worldBean.modifySync(world -> {
+            List<String> results = new ArrayList<>();
+
+            List<Aircrafts.Aircraft> aircrafts = world.aircrafts().all()
+                    .filter(a -> a.getLocationStatus() == Aircrafts.LocationStatus.ParkedAtAirport
+                            && a.getOperationalStatus() == Aircrafts.OperationalStatus.Active
+                            && a.getLocationAirportId() > 0)
+                    .toList();
+
+            aircrafts.forEach(a -> {
+                results.add(a.getId() + "\t" +
+                        a.getRegNo() + "\t" +
+                        a.getFlightMissionId() + "\t" +
+                        (a.getFlightMissionId() > 0 && vatsimTracker.getContextByFlightMissionId(a.getFlightMissionId()).isPresent()));
+            });
+
+            return Strings.join(results, '\n');
         });
     }
 
