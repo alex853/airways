9addef9c4a149138c0df4d6c4af64c498bd48937
alex853 <alexxxey.kornev@gmail.com>
2026-06-02 18:46:23 +0300

    simplifying things

--- src/main/java/net/simforge/airways2/app/pblc/MapController.java
+++ src/main/java/net/simforge/airways2/app/pblc/MapController.java
@@ -4,7 +4,6 @@ import net.simforge.airways2.app.beans.WorldRunnerBean;
 import net.simforge.airways2.app.dto.AircraftFullDto;
 import net.simforge.airways2.app.dto.AircraftMapDto;
 import net.simforge.airways2.app.tools.Id;
-import net.simforge.airways2.app.tools.Timing;
 import net.simforge.airways2.world.datamodel.Aircrafts;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -21,12 +20,10 @@ public class MapController {
     @GetMapping("/aircraft/flying")
     public List<AircraftMapDto> getFlyingAircraft() {
         // todo ak2 separate thread to build this data outside of web-request and provide caching of this big dataset
-        try (final Timing.Timer ignored = Timing.label("MapController - getFlyingAircraft")) {
-            return worldBean.read(world -> world.aircrafts()
-                    .filter(world.aircrafts().byLocationStatus(Aircrafts.LocationStatus.Flying))
-                    .map(AircraftMapDto::from)
-                    .toList());
-        }
+        return worldBean.read(world -> world.aircrafts()
+                .filter(world.aircrafts().byLocationStatus(Aircrafts.LocationStatus.Flying))
+                .map(AircraftMapDto::from)
+                .toList());
     }
 
     @GetMapping("/aircraft/details")
